0fb4949152db901c4831b64f64a6eb080628f234
Taz <tsyroid@gmail.com>
2024-09-13 20:29:25 -0700

    Inital commit

---
+++ yazi/init.lua
@@ -0,0 +1,7 @@
+-- ~/.config/yazi/init.lua
+
+-- https://github.com/yazi-rs/plugins/tree/main/full-border.yazi
+require('full-border'):setup()
+
+-- https://github.com/dedukun/relative-motions.yazi
+require('relative-motions'):setup { show_numbers = 'relative', show_motion = true }
---
+++ yazi/plugins/smart-enter.yazi/init.lua
@@ -0,0 +1,8 @@
+-- ~/.config/yazi/plugins/smart-enter.yazi/init.lua
+
+return {
+	entry = function()
+		local h = cx.active.current.hovered
+		ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
+	end,
+}
