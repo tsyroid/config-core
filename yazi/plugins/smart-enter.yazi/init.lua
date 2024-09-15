b908008d938e045dc46d57868259e84d7885a41a
Taz <tsyroid@gmail.com>
2024-09-15 12:47:47 -0700

    Fix to eliminate yazi copy/cut/paste errors (?)

--- yazi/plugins/smart-enter.yazi/init.lua
+++
@@ -1,8 +0,0 @@
--- ~/.config/yazi/plugins/smart-enter.yazi/init.lua
-
-return {
-	entry = function()
-		local h = cx.active.current.hovered
-		ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
-	end,
-}
