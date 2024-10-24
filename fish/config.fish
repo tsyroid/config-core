43a267de9d8a6cd4399b0d9f60995fc7c4261f67
Taz <tsyroid@gmail.com>
2024-10-23 18:19:10 -0700

    Fix merge conflicts after repo rename

--- fish/config.fish
+++ fish/config.fish
@@ -13,8 +13,8 @@ set -Ux EDITOR nvim
 
 set -e fish_user_paths # clear user_paths
 fish_add_path $HOME/.local/bin
-fish_add_path $HOME/.config/emacs/bin/ # Doom Emacs
-# fish_add_path /opt/homebrew/bin # macOS
+fish_add_path /opt/homebrew/bin # macOS
+# fish_add_path $HOME/.config/emacs/bin/ # Doom Emacs
 
 # if postgres
 # fish_add_path /Applications/Postgres.app/Contents/Versions/latest/bin
@@ -89,8 +89,8 @@ alias graph 'git log --all --decorate --oneline --graph'
 # alias latest 'restic -r /media/restic/home ls latest | bat'
 
 # homebrew (Linux)
-# alias bvf 'brew list --versions' # brew list (versions) formulas
-# alias bvc 'brew list --version --cask' # brew list (versions) casks
+alias bvf 'brew list --versions' # brew list (versions) formulas
+alias bvc 'brew list --version --cask' # brew list (versions) casks
 
 # VIFM
 # https://vifm.info/
