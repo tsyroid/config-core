7823488a0e1736942bbc760aabc1aa1f29aeb971
Taz <tsyroid@gmail.com>
2024-11-19 12:23:20 -0800

    Yazi: Add simple-status line

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,10 +1,13 @@
 -- ~/.config/yazi/init.lua
 
 -- https://github.com/yazi-rs/plugins/tree/main/full-border.yazi
-require("full-border"):setup()
+require('full-border'):setup()
 
 -- https://github.com/dedukun/relative-motions.yazi
-require("relative-motions"):setup({ show_numbers = "relative", show_motion = true })
+require('relative-motions'):setup { show_numbers = 'relative', show_motion = true }
 
 -- https://github.com/yazi-rs/plugins:git
-require("git"):setup()
+require('git'):setup()
+
+-- https://github.com/Ape/simple-status.yazi
+require('simple-status'):setup()
